bffd606609d045dc2e06836641c2012279f6326e
Fred <huertaserick@yahoo.ie>
2026-08-23 11:14:11 +0100

    Complete PHP arrays lesson

--- 01-php-basics/arrays.php
+++ 01-php-basics/arrays.php
@@ -10,6 +10,7 @@ $user["skills"][] = "React";
 $user["age"] = 34;
 $user["name"] = "Frederico";
 $user["skills"][1] = "TailwindCSS";
+$user["skills"][7] = "Git";
 
 unset($user["skills"][0]);
 
@@ -32,4 +33,22 @@ foreach ($user["skills"] as $index => $s) {
     echo $index . ": " . $s . "\n";
 }
 
-var_dump($user);
+foreach ($user["skills"] as $index => $whateverThisIsWeirdVariableName) {
+    echo $index . "::::: kapow - skills " . $whateverThisIsWeirdVariableName . "\n \n";
+}
+
+array_push($user["skills"], "Vue", "Angular", "Pooping");
+
+$user["skills"][5] = "WordPress GOD";
+$user["skills"][6] = "SEO Chimpanzini";
+ksort($user["skills"]);
+
+$upperCaseSkills = array_map(
+    function ($s) {
+        return strtoupper($s);
+    },
+    $user["skills"]
+);
+
+var_dump($user["skills"]);
+var_dump($upperCaseSkills);
