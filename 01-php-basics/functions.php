7bb7e5a2947691960c253103d88eabc4f971a72c
Fred <huertaserick@yahoo.ie>
2026-09-03 18:52:19 +0100

    Learn PHP functions and type declaration

--- 01-php-basics/functions.php
+++ 01-php-basics/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function greet($firstName, $lastName = "Huertas")
+function greet(string $firstName, string $lastName = "Huertas"): string
 {
     return "Hello there, $firstName $lastName";
 }
@@ -8,7 +8,7 @@ function greet($firstName, $lastName = "Huertas")
 echo greet('Frederico', 'Smith') . PHP_EOL;
 echo greet('Frederico') . PHP_EOL;
 
-function introduce($name, $role = "Web Developer")
+function introduce(string $name, string $role = "Web Developer"): string
 {
     return "Hi my name is $name and I am a $role";
 }
@@ -18,7 +18,7 @@ echo introduce("Frederico!") . PHP_EOL;
 
 // calculation
 
-function calculate($x, $y)
+function calculate(float|int $x, float|int $y)
 {
     return $x + $y;
 }
@@ -26,7 +26,7 @@ function calculate($x, $y)
 $result = calculate(15, 20);
 echo "The total bill is $result" . PHP_EOL;
 
-function calculateExampleTax($x, $y, $a = 10)
+function calculateExampleTax(float|int $x, float|int $y, float $a = 10): float|int
 {
     return $x * $y % $a;
 }
@@ -37,7 +37,7 @@ $calculatedTax2 = calculateExampleTax(10, 20,);
 echo $calculatedTax . PHP_EOL;
 echo $calculatedTax2 . PHP_EOL;
 
-function calculateTax($price, $taxPrice = 10)
+function calculateTax(float $price, float $taxPrice = 10): float|int
 {
     return $price * ($taxPrice / 100);
 }
@@ -48,13 +48,28 @@ $resultTax2 = calculateTax(1000);
 echo 'Result tax ' . $resultTax . PHP_EOL;
 echo 'Result tax ' . $resultTax2 . PHP_EOL;
 
-function multiplyNumbersOnly(int $a, int $b)
+function multiplyNumbersOnly(int $a, int $b): int
 {
     return $a * $b;
 }
 
-$whatsTheNumber = multiplyNumbersOnly(1, 2);
-$notANumber = multiplyNumbersOnly("abc", 20);
+// $whatsTheNumber = multiplyNumbersOnly(1, 2);
+// $notANumber = multiplyNumbersOnly("abc", 20);
 
-echo 'the number is ' . $whatsTheNumber;
-echo 'wrong output ' . $notANumber;
+// echo 'the number is ' . $whatsTheNumber;
+// echo 'wrong output ' . $notANumber;
+
+function isLegalToDrive(int $age): bool
+{
+    return $age >= 18;
+}
+
+$person1 = isLegalToDrive(17);
+$person2 = isLegalToDrive(22);
+
+var_dump($person1);
+var_dump($person2);
+
+echo "Heyy Katie, you think you can drive today ?" . PHP_EOL;
+echo "Katie: " . ($person1 ? 'Yes, old enought to drive' : 'Nope, not old enough') . PHP_EOL;
+echo "Willie: " . (!$person2 ? 'No, not old enough' : 'Let me drive, im old enough') . PHP_EOL;
