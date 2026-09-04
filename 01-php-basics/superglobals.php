986c0d9e403dee77ff8c2d67963ca326275f70c5
Fred <huertaserick@yahoo.ie>
2026-09-04 14:42:57 +0100

    Learn PHP superglobals and GET requests

---
+++ 01-php-basics/superglobals.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+if (isset($_GET["name"])) {
+    echo $_GET["name"] . PHP_EOL;
+}
+
+if (isset($_GET["age"])) {
+    echo $_GET["age"] . PHP_EOL;
+}
+
+// echo $_GET["name"] . PHP_EOL;
+// echo $_GET["age"] . PHP_EOL;
