520278b2225fb4e7ac79a2b8a6681dc420e97661
Fred <huertaserick@yahoo.ie>
2026-09-07 15:38:31 +0100

    Add PHP form validation and input escaping

--- 01-php-basics/validation.php
+++ 01-php-basics/validation.php
@@ -1,15 +1,28 @@
 <?php
 
 $name = $_POST["name"] ?? "";
+$email = $_POST["email"] ?? "";
+
 $error = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
     if ($name === "") {
-        echo "Name is REQUIRED DUMBASS!!!!!";
+
+        echo "Name is required.";
     } elseif (strlen($name) < 3) {
-        $error = "Name must be longer than 3 characters!!";
+
+        $error = "Name must be atleast 3 characters.";
+    } elseif ($email === "") {
+
+        $error = "Email must not be empty";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        echo "Please enter a valid email address.";
     } else {
-        echo "Hello, " . htmlspecialchars($name);
+
+        echo "Hello, " . htmlspecialchars($name) . "<br>";
+        echo "Your email address is " . htmlspecialchars($email);
     }
 }
 
@@ -17,7 +30,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 <form action="" method="POST">
 
-    <input type="text" name="name">
+    <input type="text" name="name" placeholder="Name">
+    <br>
+    <br>
+    <input type="text" name="email" placeholder="Email">
 
     <?php if ($error !== ""): ?>
         <p> <?php echo $error; ?> </p>
